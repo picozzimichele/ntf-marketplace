d5f4ec34bd1d654ee6259a73eb37540bd4dd6d5b
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-22 16:45:09 +0800

    it creates the e-mail input

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,8 @@ import { useTheme } from "next-themes";
 import images from "../assets";
 import Button from "./Button";
 
+const FooterLinks = ({ heading, items }) => {};
+
 export default function Footer() {
     const { theme } = useTheme();
 
@@ -26,6 +28,22 @@ export default function Footer() {
                     <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">
                         Get the latest updates
                     </p>
+                    <div className="flexBetween md:w-full minlg:w-577 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
+                        <input
+                            type="email"
+                            placeholder="Your Email"
+                            className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none"
+                        />
+                        <div className="flex-initial">
+                            <Button btnName={"Email Me"} classStyles={"rounded-md"} />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
+                    <FooterLinks
+                        heading="CryptoKet"
+                        items={["Explore", "How it Works", "Contact Us"]}
+                    />
                 </div>
             </div>
             <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
