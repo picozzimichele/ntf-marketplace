c8978c831de7ebc9b5b056a2b6a4119144597e06
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-22 13:44:44 +0800

    it adds the assets and starts the navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+
+import images from "../assets";
+
+export default function Navbar() {
+    const { theme, setTheme } = useTheme();
+    return (
+        <nav className="flexBetween w-full z-10 fixed p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+            {/* Logo */}
+            <div className="flex flex-1 flex-row justify-start">
+                <Link href="/">
+                    <div onClick={() => {}} className="flexCenter md:hidden cursor-pointer">
+                        <Image
+                            src={images.logo02}
+                            objectFit="contain"
+                            width={32}
+                            height={32}
+                            alt="logo"
+                        />
+                        <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
+                            CryptoKet
+                        </p>
+                    </div>
+                </Link>
+                <Link href="/">
+                    <div onClick={() => {}} className="hidden md:flex">
+                        <Image
+                            src={images.logo02}
+                            objectFit="contain"
+                            width={32}
+                            height={32}
+                            alt="logo"
+                        />
+                    </div>
+                </Link>
+            </div>
+            {/* Dark Mode Toggle */}
+            <div className="flex flex-initial flex-row justify-end">
+                <div className="flex items-center mr-2"></div>
+            </div>
+        </nav>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Footer, Navbar } from "../components";
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider attribute="class">
+            <div className="dark:bg-nft-dark bg-white min-h-screen">
+                <Navbar />
+                <Component {...pageProps} />
+                <Footer />
+            </div>
+            <Script src="https://kit.fontawesome.com/74944751be.js" crossorigin="anonymous" />
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
