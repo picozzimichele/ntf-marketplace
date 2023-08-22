2d4c9f081542ad893d2240cd21d5fc0e9578894e
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-22 14:39:55 +0800

    creating reusable button

---
+++ components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ classStyles, btnName }) {
+    return (
+        <button
+            type="button"
+            className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+        >
+            {btnName}
+        </button>
+    );
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,9 +5,55 @@ import Image from "next/image";
 import Link from "next/link";
 
 import images from "../assets";
+import Button from "./Button";
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+    const generateLink = (index) => {
+        switch (index) {
+            case 0:
+                return "/";
+            case 1:
+                return "/created-nfts";
+            case 2:
+                return "/my-nfts";
+            default:
+                return "/";
+        }
+    };
+
+    return (
+        <ul className={`list-none flexCenter ${isMobile ? "flex-col h-full" : "flex-row"}`}>
+            {["Explore NFTs", "Listed NFTs", "My NFTs"].map((item, index) => (
+                <li
+                    key={index}
+                    onClick={() => {
+                        setActive(item);
+                    }}
+                    className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-black-1 mx-3 ${
+                        active === item
+                            ? "dark:text-white text-nft-black-1"
+                            : "dark:text-nft-gray-3 text-nft-gray-2"
+                    }`}
+                >
+                    <Link href={generateLink(index)}>{item}</Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const ButtonGroup = () => {
+    const hasConnected = false;
+    return hasConnected ? (
+        <Button btnName="" classStyles="mx-2 rounded-xl" />
+    ) : (
+        <Button classStyles="mx-2 rounded-xl" />
+    );
+};
 
 export default function Navbar() {
     const { theme, setTheme } = useTheme();
+    const [active, setActive] = useState("Explore NFTs");
     return (
         <nav className="flexBetween w-full z-10 fixed p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
             {/* Logo */}
@@ -55,6 +101,12 @@ export default function Navbar() {
                         <i className="fas fa-moon" />
                         <div className="ball w-3 h-3 absolute rounded-full bg-white" />
                     </label>
+                </div>
+            </div>
+            <div className="md:hidden flex">
+                <MenuItems active={active} setActive={setActive} />
+                <div className="ml-4">
+                    <ButtonGroup />
                 </div>
             </div>
         </nav>
