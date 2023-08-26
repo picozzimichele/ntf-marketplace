45b3f2e65dbac425c2d80207ad061b74eed9d289
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-26 12:14:21 +0800

    finishes the layout of the modal

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -7,7 +7,46 @@ import { shortenAddress } from "../utils/shortenAddress";
 import { useRouter } from "next/router";
 
 const PaymentBodyCmp = ({ nft, nftCurrency }) => {
-    return <div>TEST</div>;
+    return (
+        <div className="flex flex-col">
+            <div className="flexBetween">
+                <p className="font-poppins dark:text-white text-nft-black-1 text-base font-semibold minlg:text-xl">
+                    Item
+                </p>
+                <p className="font-poppins dark:text-white text-nft-black-1 text-base font-semibold minlg:text-xl">
+                    Subtotal
+                </p>
+            </div>
+            <div className="flexBetweenStart my-5">
+                <div className="flex-1 flexStartCenter">
+                    <div className="relative h-28 w-28">
+                        <Image src={nft.image} layout="fill" objectFit="cover" />
+                    </div>
+                    <div className="flexCenterStart flex-col ml-5">
+                        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">
+                            {shortenAddress(nft.seller)}
+                        </p>
+                        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">
+                            {nft.name}
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-sm minlg:text-xl">
+                        {nft.price} <span className="font-semibold">{nftCurrency}</span>
+                    </p>
+                </div>
+            </div>
+            <div className="flexBetween mt-10">
+                <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-base minlg:text-xl">
+                    Total
+                </p>
+                <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-sm minlg:text-xl">
+                    {nft.price} <span className="font-semibold">{nftCurrency}</span>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default function NFTDetails() {
@@ -103,7 +142,7 @@ export default function NFTDetails() {
                 header="Check Out"
                 body={<PaymentBodyCmp nft={nft} nftCurrency={nftCurrency} />}
                 footer={() => <div>Custom Footer</div>}
-                handleClick={() => {}}
+                handleClose={() => {}}
             />
         </div>
     );
